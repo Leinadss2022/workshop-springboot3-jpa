fd79b6eac75272a660ead423693965be6229e256
daniel santos <ssa.dssantos@sompo.com.br>
2022-12-28 17:25:09 -0300

    Projeto created

---
+++ src/main/java/com/projetospring/portifolio/PortifolioApplication.java
@@ -0,0 +1,13 @@
+package com.projetospring.portifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetospring/portifolio/PortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospring.portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
