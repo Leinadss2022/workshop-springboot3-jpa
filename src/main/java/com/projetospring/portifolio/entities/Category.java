01b0649473f76c4ade91d3335137d9051e78a6db
Daniel Santos <leinadss2011@gmail.com>
2023-01-03 11:53:23 -0300

    Product

--- src/main/java/com/projetospring/portifolio/entities/Category.java
+++ src/main/java/com/projetospring/portifolio/entities/Category.java
@@ -1,13 +1,16 @@
 package com.projetospring.portifolio.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 
 @Entity
@@ -19,6 +22,9 @@ public class Category implements Serializable {
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
 		private String name;
+		
+		@Transient
+		private Set<Product> products = new HashSet<>();
 		
 		public Category() {
 			
@@ -41,6 +47,11 @@ public class Category implements Serializable {
 		public void setName(String name) {
 			this.name = name;
 		}
+			
+		public Set<Product> getProducts() {
+				return products;
+			}
+		
 		@Override
 		public int hashCode() {
 			return Objects.hash(id);
@@ -56,6 +67,8 @@ public class Category implements Serializable {
 			Category other = (Category) obj;
 			return id == other.id;
 		}
+	
+		
 
 						
 	
--- src/main/java/com/projetospring/portifolio/repositories/CategoryRepository.java
+++ src/main/java/com/projetospring/portifolio/repositories/CategoryRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projetospring.portifolio.entities.Category;
 
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
