b7f1e1c878d52a0ab181b7cad8bb3a7c8f6de156
Daniel Santos <leinadss2011@gmail.com>
2023-01-05 11:25:14 -0300

    Exception handling - update

--- src/main/java/com/projetospring/portifolio/services/UserService.java
+++ src/main/java/com/projetospring/portifolio/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetospring.portifolio.repositories.UserRepository;
 import com.projetospring.portifolio.services.exceptions.DatabaseException;
 import com.projetospring.portifolio.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -44,9 +46,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
