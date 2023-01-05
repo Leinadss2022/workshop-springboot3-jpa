df609a9748201f6cd26c732bc953891e384e1e89
Daniel Santos <leinadss2011@gmail.com>
2023-01-05 11:07:50 -0300

    Exception handling - delete

--- src/main/java/com/projetospring/portifolio/services/UserService.java
+++ src/main/java/com/projetospring/portifolio/services/UserService.java
@@ -4,35 +4,45 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetospring.portifolio.entities.User;
 import com.projetospring.portifolio.repositories.UserRepository;
+import com.projetospring.portifolio.services.exceptions.DatabaseException;
 import com.projetospring.portifolio.services.exceptions.ResourceNotFoundException;
-
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-		Optional<User > obj = repository.findById(id);
+		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
+
 	public User insert(User obj) {
 		return repository.save(obj);
-		
+
 	}
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
@@ -43,6 +53,6 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
---
+++ src/main/java/com/projetospring/portifolio/services/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package com.projetospring.portifolio.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
